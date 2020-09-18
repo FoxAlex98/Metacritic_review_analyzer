62bffd916891379d26f59d540b9237faea468c01
Alessandro <soralex008@gmail.com>
2020-09-19 00:31:30 +0200

    add sentiment analysis

--- spark/code/utils/data_extractors.py
+++ spark/code/utils/data_extractors.py
@@ -1,13 +1,16 @@
 import datetime
 from langdetect import detect
+import sentiment as sent
 
 def get_review(_review):
+    language = get_language(_review)
     return {
             'name': _review['name'],
             'rating': _review['rating'],
             'date': get_date(_review),
-            'language': get_language(_review),
-            'review': clean_review(_review)
+            'language': language,
+            'review': clean_review(_review),
+            'sentiment': sent.filter_review_eng(language, _review['review'])
             #'valid': get_valid()
             }
 
@@ -17,7 +20,7 @@ def clean_review(review):
 def get_date(review):
     try:
         date = review['date']
-        return datetime.datetime.strptime(date, '%b %d, %Y')
+        return datetime.datetime.strptime(date, '%b %d, %Y').date()
     except TypeError as err:
         print("///////////////////////////")
         print(err)
--- spark/code/utils/sentiment.py
+++ spark/code/utils/sentiment.py
@@ -1,1 +1,23 @@
-print("sentiment")
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
+
+def filter_review_eng(language, review):
+    if language == 'en':
+        return get_sentiment(review)
+    return "NIE"
+
+def get_sentiment(phrase):
+    polarity = analyzer.polarity_scores(phrase)         
+    if polarity["compound"] >= 0.05:
+        if polarity['neg'] > 0.05:
+            return 'ironic'
+        return 'positive'
+    elif polarity["compound"] <= -0.05:
+        if polarity['pos'] > 0.05:
+            return 'ironic'
+        return 'negative'
+    else:
+        if polarity["pos"] > 0 and polarity["neg"] > 0:
+            return "ironic"
+        return 'mixed'
